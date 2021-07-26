abf488b7364f89aaeabd40e5a920bdce807b3473
Vinicius Ventura <vinicius__20@hotmail.com>
2021-07-25 23:03:12 -0300

    add footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './footer.scss'
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <h2>Feito por <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/ventura-v/">Vinicius Ventura</a></h2>
+        </div>
+    );
+};
+
+export default Footer;
