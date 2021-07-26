23a27b9540a6464d5ab216c2799e2897339e1bf1
Vinicius Ventura <vinicius__20@hotmail.com>
2021-07-25 23:03:23 -0300

    add header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './header.scss'
+
+const Header = () => {
+    return (
+        <div className="menu">
+            
+        </div>
+    );
+};
+
+export default Header;
